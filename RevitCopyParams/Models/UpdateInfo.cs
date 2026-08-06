f699884b483792266388ecec9ffc6244e39441e1
Konstantin Tarabuikin <ktarabukin@gmail.com>
2026-08-06 19:09:47 +0300

    Добавил базовую инфраструктуру автообновления

---
+++ RevitCopyParams/Models/UpdateInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RevitCopyParams.Models
+{
+    public class UpdateInfo
+    {
+        public Version CurrentVersion { get; set; }
+
+        public Version LatestVersion { get; set; }
+
+        public string DownloadUrl { get; set; }
+
+        public bool UpdateAvailable
+        {
+            get
+            {
+                return LatestVersion > CurrentVersion;
+            }
+        }
+    }
+}
---
+++ RevitCopyParams/Services/UpdateService.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using RevitCopyParams.Models;
+using System;
+using System.Reflection;
+
+namespace RevitCopyParams.Services
+{
+    public static class UpdateService
+    {
+        public static Version GetCurrentVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            return assembly.GetName().Version;
+        }
+
+
+        public static UpdateInfo GetUpdateInfo()
+        {
+            return new UpdateInfo
+            {
+                CurrentVersion = GetCurrentVersion()
+            };
+        }
+    }
+}
